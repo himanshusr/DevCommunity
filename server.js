53e1d31607b404f17fa674b5061004160a146519
himanshusr <singhraoh@gmail.com>
2022-02-13 13:48:26 +0530

    Init commit

---
+++ server.js
@@ -0,0 +1,11 @@
+const express = require('express');
+
+const app = express();
+
+const PORT = process.env.PORT || 5000;
+
+app.get('/', (req, res) => {
+  res.send('Api Running');
+});
+
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
